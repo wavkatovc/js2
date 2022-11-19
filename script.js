b8d86b8f01ca6cfc736ed81c28570acaec1ff919
unknown <muhammadtulaganov1xbet@gmail.com>
2022-11-19 18:56:48 +0500

    js22

---
+++ script.js
@@ -0,0 +1,24 @@
+var modal = prompt("Ismingizni kiriting")
+console.log("Sizning ismingiz: " + modal);
+var age = +prompt("Yoshingizni kiriting")
+console.log("Sizning yoshingiz: " + age);
+var misol1 = +prompt("7+3=?")
+if (misol1 == 10) {
+    console.log("Siz togri javob berdingiz: " + misol1)
+} else {
+    console.log("Siz notogri javob berdingiz: " + misol1 + " ; Togri javob: 10")
+}
+
+var misol3 = +prompt("8*8=?")
+if (misol3 == 64) {
+    console.log("Siz togri javob berdingiz: " + misol3)
+} else {
+    console.log("Siz notogri javob berdingiz: " + misol3 + " ; Togri javob: 64")
+}
+
+var misol4 = +prompt("18:3=?")
+if (misol4 == 6) {
+    console.log("Siz togri javob berdingiz: " + misol4)
+} else {
+    console.log("Siz notogri javob berdingiz: " + misol4 + " ; Togri javob: 6")
+}
